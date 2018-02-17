5472a372f1cdf2c7f7d587b6c3cc4afb30ef49f8
Nikita Doroshkin <nikitadorshkin@gmail.com>
2018-02-17 12:34:42 +0300

    Initial solution

--- js/solution.js
+++ js/solution.js
@@ -9,10 +9,42 @@
      * @param {number[][]} map карта островов представленная двумерной матрицей чисел
      * @returns {number} кол-во островов
      */
+
     function solution(map) {
-        // todo: подсчитать кол-во островов на карте
-        return 0;
+        function deleteIsland(i, j) {
+            if (changeableMap[i][j] != ISLAND)
+                return;
+            changeableMap[i][j] = WATER;
+            if (i + 1 < N)
+                deleteIsland(i + 1, j);
+            if (i - 1 >= 0)
+                deleteIsland(i - 1, j);
+            if (j + 1 < N)
+                deleteIsland(i, j + 1);
+            if (j - 1 >= 0)
+                deleteIsland(i, j - 1);
+        }
+
+        var changeableMap = [];
+        for (i = 0; i < map.length; ++i)
+            changeableMap[i] = map[i].slice();
+            
+        var islandCounter = 0;
+        var N = changeableMap.length;
+        var M = changeableMap[0].length;
+
+        for (i = 0; i < N; i++) {
+            for (j = 0; j < M; j++) {
+                if (changeableMap[i][j] == ISLAND) {
+                    ++islandCounter;
+                    deleteIsland(i, j);
+                }
+            }
+        }
+        return islandCounter;
     }
+
+
 
     root.SHRI_ISLANDS.solution = solution;
 })(this);
